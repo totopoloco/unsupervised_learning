b25ed0502ec4ddfc136d1b02adc4b3816f5bbceb
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-11-21 18:13:27 +0100

    "Break free and add Gradle, tests, properties and .gitignore for unsupervised learning project"

---
+++ src/main/java/at/mavila/unsupervised/UnsupervisedLearningApplication.java
@@ -0,0 +1,13 @@
+package at.mavila.unsupervised;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsupervisedLearningApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnsupervisedLearningApplication.class, args);
+  }
+
+}
